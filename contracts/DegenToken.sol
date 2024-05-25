e304252a7c77ecc7af456f79d984509388ce4282
ramanaman12345 <54657684+ramanaman12345@users.noreply.github.com>
2024-05-26 01:23:28 +0530

    Update DegenToken.sol

--- contracts/DegenToken.sol
+++ contracts/DegenToken.sol
@@ -1,1 +1,42 @@
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MyToken is ERC20, ERC20Permit {
+    constructor() ERC20("Degen", "DGN") ERC20Permit("DGN")
+{}
+    event RedeemToken(address account, uint rewardCategory);
+    event BurnToken(address account, uint amount);
+    event TransferToken(address from, address to, uint amount);
+
+    // onlyOwner modifier allows only the user to execute the function
+    function mint(address to, uint256 amount) public
+     {
+            _mint(to, amount);
+    }
+    
+    // In-built transfer function is used
+    // transfer(to, amount);
+
+    //Wrapper function for balanceOf function of ERC20
+    function getBalance() public view returns (uint){
+        return balanceOf(msg.sender);
+    }
+
+    function redeem(uint rewardCategory) public {
+        uint requiredAmount = rewardCategory * 10;
+        require(balanceOf(msg.sender)>=requiredAmount,"Insufficient Amount");
+        burn(requiredAmount);
+        emit RedeemToken(msg.sender, rewardCategory);
+    }
+
+    // Wrapper function to access the private _burn function of ERC20
+    function burn(uint amount) public {
+        _burn(msg.sender, amount);
+        emit BurnToken(msg.sender, amount);
+    }
+}
